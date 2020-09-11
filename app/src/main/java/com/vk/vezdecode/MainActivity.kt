bd3efb9a41a9df96f9217ccd7ed0bb923dcef788
Gleb Lebedenko <RhubarbDrink@gmail.com>
2020-09-12 02:26:52 +0300

    WIP

--- app/src/main/java/com/vk/vezdecode/MainActivity.kt
+++ app/src/main/java/com/vk/vezdecode/MainActivity.kt
@@ -2,13 +2,10 @@ package com.vk.vezdecode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import androidx.appcompat.app.ActionBar
-import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.feed)
     }
 }
