fe8e1e2d1de28df0c18f5d0780d23b85408887f9
pihanya <gostevvm@yandex.ru>
2020-09-12 01:39:31 +0300

    Rename activity to FundInfoEditActivity, add class and add to manifest

---
+++ app/src/main/java/com/vk/vezdecode/FundInfoEditActivity.kt
@@ -0,0 +1,11 @@
+package com.vk.vezdecode
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FundInfoEditActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fund_info_edit_activity)
+    }
+}
