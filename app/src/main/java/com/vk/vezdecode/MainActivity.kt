fed20aa64b838f6a184f04fac1bd95679d8b0d60
Gleb Lebedenko <RhubarbDrink@gmail.com>
2020-09-11 19:16:32 +0300

    project: initial commit

---
+++ app/src/main/java/com/vk/vezdecode/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.vk.vezdecode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
